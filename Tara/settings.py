1a741eeebee7a8544b609e8b325083037d90787d
Anumol03 <anumol030101@gmail.com>
2025-02-12 16:39:41 +0530

    change email

--- Tara/settings.py
+++ Tara/settings.py
@@ -202,8 +202,8 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
-EMAIL_HOST_USER ='anumol030101@gmail.com'
-EMAIL_HOST_PASSWORD ='agwiztonuixneoip'
+EMAIL_HOST_USER ='taraihubrobotics@gmail.com'
+EMAIL_HOST_PASSWORD ='lmtpfrtjnceyrkls'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
